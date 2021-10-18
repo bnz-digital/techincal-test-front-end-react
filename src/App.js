7a4f48ba13705a7e0940754e35eb97c29d4ecf81
chrigglesby <chris@clicksuite.co.nz>
2021-10-18 13:41:43 +1300

    Fetch nav data, output top nav items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { fetchData } from './Data/data';
 
 function App() {
+  const [navItems, setNavItems] = useState([]);
+
+  useEffect(() => {
+      fetchData()
+          .then(data => setNavItems(data))
+  }, [navItems]);
+
+  const renderNav = () => {
+      // TODO: Loading state set by useEffect
+      if (!navItems.length) return 'loading...';
+
+      return navItems.map(i =>
+          <li>{ i.name }</li>
+      );
+  };
+
   return (
     <div className="App">
-
+        { renderNav() }
     </div>
   );
 }
